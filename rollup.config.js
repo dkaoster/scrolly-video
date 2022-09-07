2da9d5d193720feb4db8a7d7d3678948d50121ee
Daniel Kao <dkao@diplateevo.com>
2022-09-06 19:11:12 -0700

    Include .nojekyll

--- rollup.config.js
+++ rollup.config.js
@@ -103,7 +103,7 @@ export default [
       copy({
         targets: [
           // The public folder for development
-          { src: 'static/**/*', dest: 'build' },
+          { src: ['static/**/*', 'static/.nojekyll'], dest: 'build' },
         ],
       }),
 
