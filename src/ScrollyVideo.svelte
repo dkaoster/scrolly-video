4d3d87788caac2fb73f018f3f94a85f8ac95c68b
Daniel Kao <dkao@diplateevo.com>
2023-01-06 23:40:23 -0700

    Improve Components

--- src/ScrollyVideo.svelte
+++ src/ScrollyVideo.svelte
@@ -2,28 +2,6 @@
   import { onDestroy } from 'svelte';
   import ScrollyVideo from './ScrollyVideo';
 
-  // transitionSpeed sets the upper limit for playbackRate.
-  // Must be a number between >2 and <16.
-  export let transitionSpeed = 8;
-  export let frameThreshold = 0.1;
-
-  // Allows a way to specify display options, for instance adding 'cover'
-  // will tell this component to "cover" in its container.
-  export let cover = true;
-  export let sticky = true;
-  export let full = true;
-
-  // Use the WebCodecs API for even better performance, but will only work
-  // if the WebCodecs API is available, either natively in the browser or
-  // via a polyfill (which is not included by default)
-  export let useWebCodecs = true;
-
-  // The src of the video
-  export let src;
-
-  // Allow a debug flag
-  export let debug;
-
   // variable to hold the DOM element
   let scrollyVideoContainer;
 
@@ -34,17 +12,7 @@
     if (scrollyVideoContainer) {
       // if scrollyvideo already exists and any parameter is updated, destroy and recreate.
       if (scrollyVideo && scrollyVideo.destroy) scrollyVideo.destroy();
-      scrollyVideo = new ScrollyVideo({
-        transitionSpeed,
-        frameThreshold,
-        cover,
-        sticky,
-        full,
-        useWebCodecs,
-        src,
-        debug,
-        scrollyVideoContainer,
-      });
+      scrollyVideo = new ScrollyVideo({ ...$$props, scrollyVideoContainer });
     }
   }
 
--- src/ScrollyVideo.vue
+++ src/ScrollyVideo.vue
@@ -2,28 +2,30 @@
 import ScrollyVideo from './ScrollyVideo';
 
 export default {
-  props: [
-    'src', 'transitionSpeed', 'frameThreshold', 'cover',
-    'sticky', 'full', 'useWebCodecs', 'debug'
-  ],
   data() {
     return {
       scrollyVideo: null,
     };
   },
+  methods: {
+    refreshScrollyVideo() {
+      if (this.scrollyVideo) this.scrollyVideo.destroy();
+      this.scrollyVideo = new ScrollyVideo({
+        scrollyVideoContainer: this.$refs.containerElement,
+        ...this.$attrs
+      });
+    }
+  },
+  watch: {
+    '$attrs': {
+      handler: function() {
+        this.refreshScrollyVideo();
+      },
+      deep: true
+    }
+  },
   mounted() {
-    if (this.scrollyVideo) this.scrollyVideo.destroy();
-    this.scrollyVideo = new ScrollyVideo({
-      scrollyVideoContainer: this.$refs.containerElement,
-      src: this.src,
-      transitionSpeed: this.transitionSpeed,
-      frameThreshold: this.frameThreshold,
-      cover: this.cover,
-      sticky: this.sticky,
-      full: this.full,
-      useWebCodecs: this.useWebCodecs,
-      debug: this.debug
-    });
+    this.refreshScrollyVideo();
   },
   unmounted() {
     if (this.scrollyVideo) this.scrollyVideo.destroy();
