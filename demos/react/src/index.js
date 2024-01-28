bcace9b732c0ff9608db1888d81b82795f8a4685
tarsinzer <tarsis.maksym@gmail.com>
2024-01-28 18:42:20 +0000

    revert: demo changes

--- demos/react/src/index.js
+++ demos/react/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import './styles.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
